031f603ecb0d346a8fb70889ffca03803e8af56e
carabidaee <108757919+carabidaee@users.noreply.github.com>
2022-07-17 18:54:37 +0300

    Add files via upload

---
+++ src/components/ErrorApi.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="error-api__container">
+        Упс! Что-то пошло не так :( Попробуйте перезагрузить страницу
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ErrorApi"
+    }
+</script>
+
+<style scoped>
+    .error-api__container {
+        position: absolute;
+        top: -50px;
+        width: 100%;
+        height: 100%;
+        background: rgba(156, 8, 8, 0.6);
+        color: white;
+        text-align: center;
+        padding-top: 200px;
+        font-size: 30px;
+    }
+</style>
